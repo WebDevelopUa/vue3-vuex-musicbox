0733aa51e278a8625f96c213d28748780998da21
Serge M <sergii.mysak@vantino.ch>
2021-11-23 00:05:54 +0200

    Split modal window on Login and Register form

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -31,19 +31,33 @@
           </div>
 
           <!-- Tabs -->
+          <!--  use data() to switch between 'login' and 'register' forms        -->
           <ul class="flex flex-wrap mb-4">
             <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition hover:text-white text-white
-                bg-blue-600" href="#">Login</a>
+              <a class="block rounded py-3 px-4 transition"
+                 href="#"
+                 @click.prevent="tab = 'login'"
+                 :class="{
+                   'hover:text-white text-white bg-blue-600' : tab === 'login',
+                   'hover:text-blue-600' : tab === 'register'
+                 }"
+              >Login</a>
             </li>
             <li class="flex-auto text-center">
               <a class="block rounded py-3 px-4 transition"
-                 href="#">Register</a>
+                 href="#"
+                 @click.prevent="tab = 'register'"
+                 :class="{
+                   'hover:text-white text-white bg-blue-600' : tab === 'register',
+                   'hover:text-blue-600' : tab === 'login'
+                 }"
+              >Register</a>
             </li>
           </ul>
 
-          <!-- Login Form -->
-          <form>
+          <!-- #1 Login Form -->
+          <form v-show="tab === 'login'">
+
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -66,8 +80,10 @@
               Submit
             </button>
           </form>
-          <!-- Registration Form -->
-          <form>
+
+          <!-- #2 Registration Form -->
+          <form v-show="tab === 'register'">
+
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -129,6 +145,7 @@
               Submit
             </button>
           </form>
+
         </div>
       </div>
     </div>
@@ -141,6 +158,11 @@ import { mapMutations, mapState } from 'vuex';
 
 export default {
   name: 'Auth',
+  data() {
+    return {
+      tab: ['login', 'register'],
+    };
+  },
   computed: {
     // replaced with 'mapState' from vuex
     // authModalShow() {
