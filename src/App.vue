83d2ab7db231c91558341fe3352961728660a8e4
Serge M <sergii.mysak@vantino.ch>
2021-11-22 17:30:33 +0200

    Add Auth component

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   <AppHeader/>
 
   <Home/>
+
+  <Auth/>
 </template>
 <script>
 
 import AppHeader from '@/components/Header.vue';
 import Home from '@/views/Home.vue';
+import Auth from '@/components/Auth.vue';
 
 export default {
   name: 'App',
   components: {
+    Auth,
     Home,
     AppHeader,
   },
