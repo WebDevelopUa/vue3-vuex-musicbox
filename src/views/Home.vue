c23ba054547c9cd32e2edc5bb7d9cf6ed99abd41
Serge M <sergii.mysak@vantino.ch>
2022-01-26 19:29:57 +0200

    Add local Icon directive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,6 +28,8 @@
       <div class="bg-white rounded border border-gray-200 relative flex flex-col">
         <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
              v-icon.right.yellow="'headphones-alt'">
+          <span v-icon-secondary="{ icon:'headphones-alt', right: false }"></span>
+          <span>&emsp;</span>
           <span class="card-title">Songs</span>
           <!-- Icon replaced with v-icon directive with modifiers -->
           <!-- <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>-->
@@ -55,6 +57,7 @@ import {
   songsCollection,
 } from '@/includes/firebase';
 import SongItem from '@/components/SongItem.vue';
+import IconSecondary from '../../directives/icon-secondary';
 
 export default {
   name: 'Home',
@@ -135,6 +138,9 @@ export default {
   },
   components: {
     SongItem,
+  },
+  directives: {
+    'icon-secondary': IconSecondary,
   },
 
 };
